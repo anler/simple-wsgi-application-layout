89f52752f003ca6678cf19ccba01d5cfd407bf19
Christian Felipe Álvarez <sigilioso@gmail.com>
2011-03-23 00:05:18 +0100

    FileSystemLoader on views and models.py file added

--- simplewsgi/views.py
+++ simplewsgi/views.py
@@ -1,8 +1,13 @@
 from base import BaseView
 from http import HttpResponse
-from jinja2 import Environment, PackageLoader
+#from jinja2 import Environment, PackageLoader
+from jinja2 import Environment, FileSystemLoader
+from os import path
 
-jinja_env = Environment(loader=PackageLoader('simplewsgi', 'templates'),
+TEMPLATE_PATH = path.join(path.dirname(__file__), 'templates')
+#jinja_env = Environment(loader=PackageLoader('simplewsgi', 'templates'),
+#                        extensions=['jinja2.ext.i18n'])
+jinja_env = Environment(loader=FileSystemLoader(TEMPLATE_PATH),
                         extensions=['jinja2.ext.i18n'])
 
 class IndexView(BaseView):
